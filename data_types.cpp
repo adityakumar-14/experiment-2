fce65dd26825ee6ba4c29783bda9e95b1cc09ffe
adityakumar-14 <hellx1405@gmail.com>
2024-09-04 10:39:50 +0530

    Create data_types.cpp

---
+++ data_types.cpp
@@ -0,0 +1,32 @@
+//Aditya Kumar//
+//23070123009//
+#include<iostream>
+using namespace std;
+
+int main(){
+    int myintvar;
+    float myfloatvar;
+    double mydoublevar;
+    char mycharvar;
+    string mystringvar;
+    bool myboolvar;
+
+    cout <<"enter integer";
+    cin>>myintvar;
+
+    cout <<"enter float";
+    cin>>myfloatvar;
+
+    cout <<"enter double";
+    cin>>mydoublevar;
+
+    cout <<"enter character";
+    cin>>mycharvar;
+
+    cout<<"enter string "; 
+    cin>>mystringvar;
+
+    cout<<"enter bool" ;
+    cin>>myboolvar;
+
+}
